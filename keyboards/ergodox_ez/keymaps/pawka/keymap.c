34d0278c5a9dedafa06ff2d23bd3e8df0609b506
Povilas Balzaravicius Pawka <povilas@balzaravicius.lt>
2021-04-29 16:02:20 +0300

    Ctrl instead of Caps Lock

--- keyboards/ergodox_ez/keymaps/pawka/keymap.c
+++ keyboards/ergodox_ez/keymaps/pawka/keymap.c
@@ -1,30 +1,5 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
-/* #include "keymap_german.h" */
-/* #include "keymap_nordic.h" */
-/* #include "keymap_french.h" */
-/* #include "keymap_spanish.h" */
-/* #include "keymap_hungarian.h" */
-/* #include "keymap_swedish.h" */
-/* #include "keymap_br_abnt2.h" */
-/* #include "keymap_canadian_multilingual.h" */
-/* #include "keymap_german_ch.h" */
-/* #include "keymap_jp.h" */
-/* #include "keymap_korean.h" */
-/* #include "keymap_bepo.h" */
-/* #include "keymap_italian.h" */
-/* #include "keymap_slovenian.h" */
-/* #include "keymap_lithuanian_azerty.h" */
-/* #include "keymap_danish.h" */
-/* #include "keymap_norwegian.h" */
-/* #include "keymap_portuguese.h" */
-/* #include "keymap_contributions.h" */
-/* #include "keymap_czech.h" */
-/* #include "keymap_romanian.h" */
-/* #include "keymap_russian.h" */
-#include "keymap_uk.h"
-/* #include "keymap_estonian.h" */
-/* #include "keymap_belgian.h" */
 #include "keymap_us_international.h"
 
 #define KC_MAC_UNDO LGUI(KC_Z)
@@ -47,17 +22,13 @@ enum custom_keycodes {
   RGB_SLD = EZ_SAFE_RANGE,
 };
 
-enum tap_dance_codes {
-  DANCE_0,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
     KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(2),                                          TG(2),          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
-    LT(2,KC_ESCAPE),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           LT(2,KC_SCOLON),KC_QUOTE,
-    KC_LSHIFT,      LCTL_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,           KC_RALT,                                        KC_RALT,        KC_N,           KC_M,           KC_COMMA,       KC_DOT,         RCTL_T(KC_SLASH),KC_RSHIFT,
-    KC_LCTRL,       TD(DANCE_0),    KC_LALT,        OSL(3),         LT(1,KC_NO),                                                                                                    LT(1,KC_NO),    KC_RALT,        KC_TRANSPARENT, KC_TRANSPARENT, KC_RCTRL,
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_RALT,                                        KC_RALT,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
+    KC_LCTRL,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
+    KC_LSHIFT,      LCTL_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,           LT(2,KC_NO),                                    TG(2),          KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,
+    KC_LCTRL,       KC_LGUI,        KC_LALT,        KC_TRANSPARENT, OSL(3),                                                                                                         LT(1,KC_NO),    KC_RALT,        KC_LBRACKET,    KC_RBRACKET,    KC_RCTRL,
                                                                                                     LALT_T(KC_APPLICATION),KC_LGUI,        KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                                     KC_HOME,        KC_PGUP,
                                                                                     KC_SPACE,       KC_BSPACE,      KC_END,         KC_PGDOWN,      KC_TAB,         KC_ENTER
@@ -94,7 +65,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case LT(1,KC_NO):
@@ -115,112 +85,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-uint32_t layer_state_set_user(uint32_t state) {
-  uint8_t layer = biton32(state);
-  ergodox_board_led_off();
-  ergodox_right_led_1_off();
-  ergodox_right_led_2_off();
-  ergodox_right_led_3_off();
-  switch (layer) {
-    case 1:
-      ergodox_right_led_1_on();
-      break;
-    case 2:
-      ergodox_right_led_2_on();
-      break;
-    case 3:
-      ergodox_right_led_3_on();
-      break;
-    case 4:
-      ergodox_right_led_1_on();
-      ergodox_right_led_2_on();
-      break;
-    case 5:
-      ergodox_right_led_1_on();
-      ergodox_right_led_3_on();
-      break;
-    case 6:
-      ergodox_right_led_2_on();
-      ergodox_right_led_3_on();
-      break;
-    case 7:
-      ergodox_right_led_1_on();
-      ergodox_right_led_2_on();
-      ergodox_right_led_3_on();
-      break;
-    default:
-      break;
-  }
-  return state;
-};
-
-typedef struct {
-    bool is_press_action;
-    uint8_t step;
-} tap;
-
-enum {
-    SINGLE_TAP = 1,
-    SINGLE_HOLD,
-    DOUBLE_TAP,
-    DOUBLE_HOLD,
-    DOUBLE_SINGLE_TAP,
-    MORE_TAPS
-};
-
-static tap dance_state[1];
-
-uint8_t dance_step(qk_tap_dance_state_t *state);
-
-uint8_t dance_step(qk_tap_dance_state_t *state) {
-    if (state->count == 1) {
-        if (state->interrupted || !state->pressed) return SINGLE_TAP;
-        else return SINGLE_HOLD;
-    } else if (state->count == 2) {
-        if (state->interrupted) return DOUBLE_SINGLE_TAP;
-        else if (state->pressed) return DOUBLE_HOLD;
-        else return DOUBLE_TAP;
-    }
-    return MORE_TAPS;
-}
-
-
-void on_dance_0(qk_tap_dance_state_t *state, void *user_data);
-void dance_0_finished(qk_tap_dance_state_t *state, void *user_data);
-void dance_0_reset(qk_tap_dance_state_t *state, void *user_data);
-
-void on_dance_0(qk_tap_dance_state_t *state, void *user_data) {
-    if(state->count == 3) {
-        tap_code16(KC_GRAVE);
-        tap_code16(KC_GRAVE);
-        tap_code16(KC_GRAVE);
-    }
-    if(state->count > 3) {
-        tap_code16(KC_GRAVE);
-    }
-}
-
-void dance_0_finished(qk_tap_dance_state_t *state, void *user_data) {
-    dance_state[0].step = dance_step(state);
-    switch (dance_state[0].step) {
-        case SINGLE_TAP: register_code16(KC_GRAVE); break;
-        case SINGLE_HOLD: register_code16(KC_LGUI); break;
-        case DOUBLE_TAP: register_code16(KC_GRAVE); register_code16(KC_GRAVE); break;
-        case DOUBLE_SINGLE_TAP: tap_code16(KC_GRAVE); register_code16(KC_GRAVE);
-    }
-}
-
-void dance_0_reset(qk_tap_dance_state_t *state, void *user_data) {
-    wait_ms(10);
-    switch (dance_state[0].step) {
-        case SINGLE_TAP: unregister_code16(KC_GRAVE); break;
-        case SINGLE_HOLD: unregister_code16(KC_LGUI); break;
-        case DOUBLE_TAP: unregister_code16(KC_GRAVE); break;
-        case DOUBLE_SINGLE_TAP: unregister_code16(KC_GRAVE); break;
-    }
-    dance_state[0].step = 0;
-}
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-        [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_0, dance_0_finished, dance_0_reset),
-};
+qk_tap_dance_action_t tap_dance_actions[] = {};
