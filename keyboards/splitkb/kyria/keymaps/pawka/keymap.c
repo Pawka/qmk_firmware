d2794c53cb7abfd2a3fab38f20bb680c129cf3f7
Povilas Balzaravicius Pawka <povilas@balzaravicius.lt>
2021-10-05 10:37:44 +0300

    kyria: custom tapping terms

--- keyboards/splitkb/kyria/keymaps/pawka/keymap.c
+++ keyboards/splitkb/kyria/keymaps/pawka/keymap.c
@@ -163,3 +163,17 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     return true;
 }
 #endif
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LALT_T(KC_S):
+        case RCTL_T(KC_L):
+            return TAPPING_TERM - 30;
+        case LGUI_T(KC_A):
+        case RGUI_T(KC_SCOLON):
+            return TAPPING_TERM + 80;
+        default:
+            return TAPPING_TERM;
+
+    }
+}
