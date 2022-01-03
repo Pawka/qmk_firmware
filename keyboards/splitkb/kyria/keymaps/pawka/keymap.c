20b540979b7c16c5a1b88c7d0a5f243061b4333e
Povilas Balzaravicius Pawka <povilas@balzaravicius.lt>
2022-01-03 10:22:29 +0200

    kyria: removed numpad layer

--- keyboards/splitkb/kyria/keymaps/pawka/keymap.c
+++ keyboards/splitkb/kyria/keymaps/pawka/keymap.c
@@ -23,7 +23,6 @@ enum layers {
     _MOUSE,
     _LOCALE,
     _MEDIA,
-    _NUMNEW,
 };
 
 // Tap Dance declarations
@@ -43,7 +42,7 @@ enum {
 #define ALT_ENT  MT(MOD_LALT, KC_ENT)
 
 // Layers
-#define LT_ENT   LT(_NUMNEW, KC_ENT)
+#define LT_ENT   LT(_NUM, KC_ENT)
 #define LT_BSPC  LT(_SYM, KC_BSPC)
 #define LT_DEL   LT(_LOCALE, KC_DEL)
 #define LT_TAB   LT(_MOUSE, KC_TAB)
@@ -102,13 +101,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
-    [_NUM] = LAYOUT(
-     _______, KC_ASTR , KC_7 , KC_8 , KC_9 , KC_UNDS ,                                     _______, _______, _______, _______, _______, _______,
-     _______, KC_PLUS , KC_4,  KC_5 , KC_6 , KC_MINS ,                                     _______, _______, _______, _______, _______, _______,
-     _______, KC_SLSH , KC_1,  KC_2 , KC_3 , KC_AMPR , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                 _______, _______, KC_DOT, KC_0, KC_EQL, _______, _______, _______, _______, _______
-    ),
-
     [_MOUSE] = LAYOUT(
       _______, _______, _______, _______, _______, _______,                                     REDO, KC_PASTE, KC_MS_U, KC_CUT, KC_UNDO, RESET,
       _______, _______, _______, _______, _______, _______,                                     _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,
@@ -116,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  _______, _______, _______, _______, _______, KC_MS_BTN1, KC_MS_BTN2, KC_MS_BTN3, _______, _______
     ),
 
-    [_NUMNEW] = LAYOUT(
+    [_NUM] = LAYOUT(
       _______,  KC_1  ,  KC_2  ,  KC_3  ,  KC_4  ,  KC_5  ,                                      KC_6  ,  KC_7  ,  KC_8  ,  KC_9  , KC_0   , KC_MINS,
       _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 ,  KC_F5 ,                                      KC_F6 ,  KC_F7 ,  KC_F8 ,  KC_F9 , KC_F10 , KC_F11 ,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F12 ,
