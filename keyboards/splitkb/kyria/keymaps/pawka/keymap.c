af3d97f98a72eec2e1ed4a62d899ce300fc8202c
Povilas Balzaravičius <povilas@balzaravicius.lt>
2022-01-10 10:50:44 +0200

    kyria: add pipe symbol to the right place

--- keyboards/splitkb/kyria/keymaps/pawka/keymap.c
+++ keyboards/splitkb/kyria/keymaps/pawka/keymap.c
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_SYM] = LAYOUT(
-     _______ , KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,                                     _______, KC_UNDS, KC_PLUS, KC_ASTR, _______, _______,
+     _______ , KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,                                     KC_PIPE, KC_UNDS, KC_PLUS, KC_ASTR, _______, _______,
      _______ , KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRAVE,                                    KC_AMPR, KC_EQL , KC_MINS, KC_SLSH, _______, _______,
      _______ , KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
